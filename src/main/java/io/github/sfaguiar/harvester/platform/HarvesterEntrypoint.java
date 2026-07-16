319c21c6ec36aa9c324e80edec05b200da577029
Samuel Aguiar <samuelfaguiar107@gmail.com>
2026-07-16 19:51:23 -0300

    refactor: use the qualified logging facade

--- src/main/java/io/github/sfaguiar/harvester/platform/HarvesterEntrypoint.java
+++ src/main/java/io/github/sfaguiar/harvester/platform/HarvesterEntrypoint.java
@@ -1,12 +1,12 @@
 package io.github.sfaguiar.harvester.platform;
 
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public final class HarvesterEntrypoint implements ModInitializer {
     public static final String MOD_ID = "harvester";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
